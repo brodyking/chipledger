debf9296d25cf68443e6d8dc38d21be14fc5acac
brody king <46272894+brodyking@users.noreply.github.com>
2025-06-05 16:34:10 -0500

    Lists all games owned by that persons account

--- pages/games.php
+++ pages/games.php
@@ -24,6 +24,50 @@
             </h2>
             <div class="card-body">
 
+                <table class="table w-100 table-striped border" style="font-size:1.15em;">
+                    <?php
+
+
+                    try {
+
+                        $db = new PDO("sqlite:" . $config["databaseLocation"]);
+                        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Good practice to set error mode
+                    
+                        $sql = "SELECT * FROM games WHERE username=:usernameInput";
+
+                        $statement = $db->prepare($sql);
+
+                        $username = filter_input(INPUT_COOKIE, 'username');
+                        $statement->bindValue(":usernameInput", $username, PDO::PARAM_STR);
+
+                        $statement->execute(); // <--- YOU WERE MISSING THIS LINE!
+                    
+                        $r = $statement->fetchAll(PDO::FETCH_ASSOC); // Fetch as an associative array
+                    
+                        $db = null; // Close the database connection
+                    
+                        $table = "";
+
+                        if ($r) {
+                            foreach ($r as $game) {
+                                $table = "<tr class='w-100'><td><a class='text-decoration-none' href='/view?game={$game["name"]}'>{$game['name']} <i class='bi bi-arrow-right-circle-fill'></i></a></td></tr>" . $table;
+                            }
+                            echo $table;
+                        }
+
+                    } catch (PDOException $e) {
+                        Header("Location: /login?error=Unknown Error");
+                        //echo "Error: " . $e->getMessage(); // Display a more user-friendly error
+                        die();
+                    }
+
+
+
+
+
+                    ?>
+                </table>
+
             </div>
         </div>
 
@@ -40,8 +84,8 @@
                         <div class="modal-body">
                             <div class="mb-3">
                                 <label class="form-label">Game Name <span class="text-danger">*</span></label>
-                                <input type="text" class="form-control" name="name"
-                                    value="<?php echo date("m-d-Y"); ?>" required>
+                                <input type="text" class="form-control" name="name" value="<?php echo $username . "-" . date("m-d-Y");
+                                ?>" required>
                             </div>
                         </div>
                         <div class="modal-footer">
