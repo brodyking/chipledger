06748e4df31d7974c96f06411330853cad9ee2e0
brody king <46272894+brodyking@users.noreply.github.com>
2025-06-05 23:56:06 -0500

    Add new players

---
+++ api/v1/game/addPlayer.php
@@ -0,0 +1,98 @@
+<?php
+
+if (isset($_COOKIE["username"]) && isset($_COOKIE["session"]) && isset($_GET["name"]) && isset($_GET["playername"])) {
+
+    include_once "scripts/isloggedin.php";
+
+    if (!isloggedin($config["databaseLocation"])) {
+        echo json_encode(array("error" => true, "errormessage" => "Not Authenticated"));
+        die();
+    }
+
+    if ($_GET["name"] == "" || $_GET["playername"] == "") {
+        echo json_encode(array("error" => true, "errormessage" => "Name cannot be empty"));
+        die();
+    }
+
+
+    try {
+
+        $db = new PDO("sqlite:" . $config["databaseLocation"]);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Good practice to set error mode
+
+        $sql = "SELECT * FROM games WHERE name=:nameInput AND username=:usernameInput";
+
+        $statement = $db->prepare($sql);
+
+        $username = filter_input(INPUT_COOKIE, 'username');
+        $statement->bindValue(":usernameInput", $username, PDO::PARAM_STR);
+
+        $name = filter_input(INPUT_GET, 'name');
+        $statement->bindValue(":nameInput", $name, PDO::PARAM_STR);
+
+        $statement->execute(); // <--- YOU WERE MISSING THIS LINE!
+
+        $r = $statement->fetch(PDO::FETCH_ASSOC); // Fetch as an associative array
+
+        $db = null; // Close the database connection
+
+        if (!$r) {
+            echo json_encode(array("error" => true, "errormessage" => "Unknown Error"));
+            exit();
+        }
+
+    } catch (PDOException $e) {
+        echo json_encode(array("error" => true, "errormessage" => "Unknown Error"));
+        //echo "Error: " . $e->getMessage(); // Display a more user-friendly error
+        die();
+    }
+
+    $data = json_decode($r["data"], true);
+
+    if (isset($data["players"][filter_input(INPUT_GET, "playername")])) {
+        echo json_encode(array("error" => true, "errormessage" => "Player already exists"));
+        die();
+    }
+
+    $data["players"][filter_input(INPUT_GET, "playername")] = 0;
+    $data["totalPlayers"] += 1;
+
+    $data = json_encode($data);
+
+    try {
+
+        $db = new PDO("sqlite:" . $config["databaseLocation"]);
+
+        $sql = "UPDATE games SET data = :dataInput WHERE (username=:usernameInput AND name=:nameInput)";
+
+        $statement = $db->prepare($sql);
+
+        $username = filter_input(INPUT_COOKIE, 'username');
+        $statement->bindValue(":usernameInput", $username, PDO::PARAM_STR);
+
+        $name = filter_input(INPUT_GET, 'name');
+        $statement->bindValue(":nameInput", $name, PDO::PARAM_STR);
+
+        $statement->bindValue(":dataInput", $data, PDO::PARAM_STR);
+
+
+        $success = $statement->execute();
+        $db = null;
+
+        if ($success) {
+            echo $data;
+        } else {
+            echo json_encode(array("error" => true, "errormessage" => "Unknown Error"));
+            die();
+        }
+
+    } catch (PDOException $e) {
+        //echo $e;
+        echo json_encode(array("error" => true, "errormessage" => "Unknown Error"));
+        die();
+    }
+
+} else {
+    echo json_encode(array("error" => true, "errormessage" => "Unknown Error"));
+    die();
+}
