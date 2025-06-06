7111311d275a141bbd057b232ee18cee3081fb88
brody king <46272894+brodyking@users.noreply.github.com>
2025-06-05 23:58:06 -0500

    Removed duplicate close button for modal

--- pages/games.php
+++ pages/games.php
@@ -79,7 +79,6 @@
                 <div class="modal-content">
                     <div class="modal-header">
                         <h1 class="modal-title fs-5">New Game</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
 
                     <form method="POST" action="/api/v1/game/new">
