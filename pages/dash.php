fb493043400213f3acdc2ef3db6cdf924a62d194
brody king <46272894+brodyking@users.noreply.github.com>
2025-06-05 23:58:13 -0500

    Release notes box

--- pages/dash.php
+++ pages/dash.php
@@ -18,6 +18,12 @@
     <main id="main">
         <?php include "components/alert.php"; ?>
         <h1 class="text-center mt-5">Welcome back, <span class="text-info"><?php echo $username; ?></span>.</h1>
+        <div class="card mt-5">
+            <h5 class="card-header">Release Notes</h5>
+            <div class="card-body">
+                WILL IMPLEMENT LATER
+            </div>
+        </div>
     </main>
     <?php include "components/footer.php"; ?>
     <script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
