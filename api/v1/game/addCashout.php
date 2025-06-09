1d3f2b8a8999bafe5d9ae0859ed6ca06dd1c2941
brody king <46272894+brodyking@users.noreply.github.com>
2025-06-08 22:14:01 -0500

    Cashout histories

--- api/v1/game/addCashout.php
+++ api/v1/game/addCashout.php
@@ -69,7 +69,8 @@ if (isset($_COOKIE["username"]) && isset($_COOKIE["session"]) && isset($_GET["na
     $data["totalPot"] -= filter_input(INPUT_GET, "amount");
     $data["cashouts"][filter_input(INPUT_GET, "playername")] += filter_input(INPUT_GET, "amount");
 
-    array_push($data["history"], ["type" => "cashout", "message" => "<b>" . filter_input(INPUT_GET, "playername") . "</b> has cashed out for <b>$" . filter_input(INPUT_GET, "amount") . "</b>"]);
+    array_push($data["totalHistory"], ["type" => "cashout", "message" => "<b>" . filter_input(INPUT_GET, "playername") . "</b> has cashed out for <b>$" . filter_input(INPUT_GET, "amount") . "</b>"]);
+    array_push($data["cashoutsHistory"], ["name" => filter_input(INPUT_GET, "playername"), "value" => filter_input(INPUT_GET, "amount")]);
 
     $data = json_encode($data);
 
