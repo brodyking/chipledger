2572631e3202d3f44400d741449e920419b54a54
brody king <46272894+brodyking@users.noreply.github.com>
2025-06-09 23:57:28 -0500

    Sends data of the game without doing any action

---
+++ api/game/get.php
@@ -0,0 +1,54 @@
+<?php
+
+if (isset($_COOKIE["username"]) && isset($_COOKIE["session"]) && isset($_GET["name"])) {
+
+    if (!isloggedin($config["database.location"])) {
+        echo json_encode(array("error" => true, "errormessage" => "Not Authenticated"));
+        die();
+    }
+
+    if ($_GET["name"] == "") {
+        echo json_encode(array("error" => true, "errormessage" => "Name cannot be empty"));
+        die();
+    }
+
+
+    try {
+
+        $db = new PDO("sqlite:" . $config["database.location"]);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Good practice to set error mode
+
+        $sql = "SELECT * FROM games WHERE name=:nameInput AND username=:usernameInput";
+
+        $statement = $db->prepare($sql);
+
+        $username = filter_input(INPUT_COOKIE, 'username');
+        $statement->bindValue(":usernameInput", $username, PDO::PARAM_STR);
+
+        $name = filter_input(INPUT_GET, 'name');
+        $statement->bindValue(":nameInput", $name, PDO::PARAM_STR);
+
+        $statement->execute(); // <--- YOU WERE MISSING THIS LINE!
+
+        $r = $statement->fetch(PDO::FETCH_ASSOC); // Fetch as an associative array
+
+        $db = null; // Close the database connection
+
+        if (!$r) {
+            echo json_encode(array("error" => true, "errormessage" => "Unknown Error"));
+            exit();
+        }
+
+    } catch (PDOException $e) {
+        echo json_encode(array("error" => true, "errormessage" => "Unknown Error"));
+        //echo "Error: " . $e->getMessage(); // Display a more user-friendly error
+        die();
+    }
+
+    echo $r["data"];
+
+
+} else {
+    echo json_encode(array("error" => true, "errormessage" => "Unknown Error"));
+    die();
+}
