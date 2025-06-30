bf2eaef094692b1296a5cf8a459d2d5d5ea3a1e5
brody king <46272894+brodyking@users.noreply.github.com>
2025-06-29 21:32:40 -0500

    Manifest at root

--- app.php
+++ app.php
@@ -34,7 +34,7 @@
     <meta name="author" content="Brody King">
 
     <!--- PWA and Icons --->
-    <link rel="manifest" href="manifest.json" />
+    <link rel="manifest" href="/manifest.json" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/x-icon" href="/lib/chipledger/img/logo.png">
