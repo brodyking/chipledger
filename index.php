78e98f210a51632eb84cab46d4caeebf28dadebf
brody king <46272894+brodyking@users.noreply.github.com>
2025-06-29 21:17:43 -0500

    TOS Link

--- index.php
+++ index.php
@@ -49,7 +49,8 @@ $docRoutes = [
     "/docs" => "/lib/chipledger/docs/index.html",
     "/docs/about-this-project" => "/lib/chipledger/docs/about-this-project.html",
     "/docs/api" => "/lib/chipledger/docs/api.html",
-    "/docs/javascript-and-rendering" => "/lib/chipledger/docs/javascript-and-rendering.html"
+    "/docs/javascript-and-rendering" => "/lib/chipledger/docs/javascript-and-rendering.html",
+    "/docs/tos" => "/lib/chipledger/docs/tos.html"
 ];
 
 // Removes extra "/" at the end of the request string if present.
