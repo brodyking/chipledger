fbd77d2bee9b0b147fa7bdc61adc428d11be5208
Brody King <46272894+brodyking@users.noreply.github.com>
2025-07-17 02:14:36 -0500

    New location of docs

---
+++ lib/chipledger/static/html/docs/top.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <!--
+     
+    Chipledger Source © 2025 Brody King
+    https://github.com/brodyking/chipledger
+    https://chipledger.com
+
+    -->
+
+    <!--- Styles --->
+    <link href="/lib/bootswatch-5/dist/lumen/bootstrap.min.css" rel="stylesheet">
+    <link href="/lib/bootstrap/icons/font/bootstrap-icons.min.css" rel="stylesheet">
+    <link href="/lib/chipledger/util/css/main.css" rel="stylesheet">
+    <link href="/lib/chipledger/static/css/static.css" rel="stylesheet">
+
+    <!--- Title --->
+    <title>
+        Chipledger Docs
+    </title>
+
+    <!--- Meta Tags --->
+    <meta name="title" content="Chipledger Docs">
+    <meta name="description" content="Chipledger's Docs. Here you can find useful information on this opensource software, and how to develop for it.">
+    <meta name="keywords" content="">
+    <meta name="robots" content="index, follow">
+    <meta name="language" content="English">
+    <meta name="revisit-after" content="30 days">
+    <meta name="author" content="Brody King">
+
+    <!--- PWA and Icons --->
+    <link rel="manifest" href="/lib/chipledger/util/manifest.json" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="/lib/chipledger/util/img/logo.png">
+    <link href="/lib/chipledger/util/img/logo-mobile.png" rel="apple-touch-icon" sizes="180x180">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="Pouchtrack">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta name="theme-color" content="#158CBA">
+
+</head>
+
+<body data-bs-theme="dark">
+    <nav class="navbar fixed-top navbar-expand-lg p-0 bg-primary" id="navbar">
+        <div class="container-fluid">
+            <span class="navbar-brand text-body-tertiary">
+                <a href="/" class="text-white text-decoration-none"><i class="bi bi-bank me-1"></i> Chipledger</a> / <a href="/docs"
+                    class="text-white text-decoration-none">Docs</a>
+            </span>
+            <a class="navbar-toggler bg-white text-black btn btn-light p-1 ps-2 pe-2" type="button" href="/">
+                <i class="bi bi-arrow-left-short text-secondary"></i>
+            </a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link" style="color:#fff;" aria-current="page"
+                    href="https://github.com/brodyking/chipledger/"><i
+                    class="align-middle bi bi-github me-1"></i></a></li>
+                    <li class="nav-item"><a class="nav-link" style="color:#fff;" aria-current="page"
+                    href="https://benadryl.dev"><i class="align-middle bi bi-capsule me-1"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
